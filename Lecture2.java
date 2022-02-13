43ad83194ea722112657bbaeca588cbb156fe691
churchil15 <churchil.rajpal15@gmail.com>
2022-02-13 15:24:08 +0530

    added Lecture2.java

---
+++ Lecture2.java
@@ -0,0 +1,57 @@
+public class Lecture2{
+    public static void main(String[] args) {
+        System.out.println("Namaste Dunia :-)\n");
+
+        int a = 123;
+        System.out.println(a);
+
+        char b = 'v';
+        System.out.println(b);
+
+        boolean b1 = true;
+        System.out.println(b1);
+
+        float f = 1.2f;
+        System.out.println(f);
+
+        double d = 1.23;
+        System.out.println(d);
+
+        int size = Double.BYTES;
+        System.out.println("Size of d is: " + size);
+
+        int a1 = 'a';
+        System.out.println(a1);
+
+        char c1 = 98;
+        System.out.println(c1);
+
+        // char c2 = 123456;
+        // System.out.println(c2);
+        
+        int a2 = 2/5;
+        System.out.println(a2);
+        System.out.println(2.0/5);
+
+        int a3 = 2;
+        int b3 = 3;
+        boolean first = (a3 == b3);
+        System.out.println(first);  
+
+        boolean second = !(a3 > b3);
+        System.out.println(second); 
+
+        boolean third = (a3 < b3);
+        System.out.println(third);
+
+        boolean fourth = (a3 >= b3);
+        System.out.println(fourth);
+
+        boolean fifth = (a3 <= b3);
+        System.out.println(fifth);
+
+        boolean six = (a3 != b3);
+        System.out.println(six);
+        
+    }
+}
