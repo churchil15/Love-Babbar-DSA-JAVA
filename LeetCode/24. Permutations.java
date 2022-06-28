cd5040ddeeb07a85aedcb97186fe9694889d37b4
Churchil Yash Rajpal <85837940+churchil15@users.noreply.github.com>
2022-06-28 20:24:01 +0530

    Create 24. Permutations.java

---
+++ LeetCode/24. Permutations.java
@@ -0,0 +1,39 @@
+class Solution {
+    public static void solve(int[] nums, List<List<Integer>> ans, int index){
+        
+        // base case
+        if(index >= nums.length){
+            
+            List<Integer> output = new ArrayList<>();
+            for(int i = 0;i<nums.length;i++){
+                
+                output.add(nums[i]);
+            }
+
+            ans.add(output);
+            return;
+        }
+        
+        for(int i = index;i<nums.length;i++){
+            int temp = nums[index];
+            nums[index] = nums[i];
+            nums[i] = temp;
+            
+            solve(nums, ans, index+1);
+            
+            // backtracking (again swapping)
+            int temp2 = nums[index];
+            nums[index] = nums[i];
+            nums[i] = temp2;
+        }
+    }
+    
+    public List<List<Integer>> permute(int[] nums) {
+        
+        List<List<Integer>> ans = new ArrayList<>();
+        int index = 0;
+        solve(nums, ans, index);
+        return ans;
+        
+    }
+}
